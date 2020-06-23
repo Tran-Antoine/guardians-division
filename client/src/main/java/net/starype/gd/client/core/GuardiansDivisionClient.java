95072f0bace7ebd01d6575a45fc5ad9a1d8687bf
Antoine Tran <antoine2001.tran@gmail.com>
2020-06-23 19:53:47 +0200

    chore: initial structure of the project

---
+++ client/src/main/java/net/starype/gd/client/core/GuardiansDivisionClient.java
@@ -0,0 +1,19 @@
+package net.starype.gd.client.core;
+
+import com.jme3.app.SimpleApplication;
+
+public class GuardiansDivisionClient extends SimpleApplication {
+
+    public static void main(String[] args) {
+        new GuardiansDivisionClient();
+    }
+
+    private GuardiansDivisionClient() {
+        super.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+
+    }
+}
